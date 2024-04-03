6a1a75a622c107d8abb7ed5f39e67c641ef73946
SaschaKohler <sascha.kohler@sent.at>
2024-04-03 10:15:23 +0200

    feat(create postuploadscreen): -

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import HomeScreen from './src/screens/HomeScreen';
 import CommentsScreen from './src/screens/CommentsScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
 import EditProfileScreen from './src/screens/EditProfileScreen';
+import PostUploadScreen from './src/screens/PostUploadScreen';
 
 const App = () => {
   return (
@@ -10,7 +11,8 @@ const App = () => {
       {/* <HomeScreen /> */}
       {/* <CommentsScreen /> */}
       {/* <ProfileScreen /> */}
-      <EditProfileScreen />
+      {/* <EditProfileScreen /> */}
+      <PostUploadScreen />
     </SafeAreaView>
   );
 };
